45db549c0c3d9441f4cb13fa67e7dc469ce509bc
Essa Ahmed <essaahmedsiddiqui@gmail.com>
2023-03-22 00:52:10 +0500

    Added Project

---
+++ 4-Shopping List/script.js
@@ -0,0 +1,95 @@
+const itemForm = document.getElementById("item-form");
+const itemInput = document.getElementById("item-input");
+const itemList = document.getElementById("item-list");
+const clearButton = document.getElementById("clear");
+const itemFilter = document.getElementById("filter");
+
+function addItem(e) {
+  e.preventDefault();
+
+  const newItem = itemInput.value;
+  // Validate Input
+  if (newItem === "") {
+    alert("Please add an item");
+    return;
+  }
+  //* Create list item
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(newItem));
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
+
+  //? Add li to DOM
+  itemList.appendChild(li);
+
+  checkUI();
+  itemInput.value = "";
+}
+
+// ? Create Button Function
+function createButton(classes) {
+  const button = document.createElement("button");
+  button.className = classes;
+  const icon = createIcon("fa-soild fa-xmark");
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement("i");
+  icon.className = classes;
+  return icon;
+}
+
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    if (confirm("Are you sure?")) {
+      e.target.parentElement.parentElement.remove();
+      checkUI();
+    }
+  }
+}
+
+function clearItems() {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+  checkUI();
+
+  //  itemList.innerHTML = ''
+}
+
+function filterItems(e) {
+  const items = itemList.querySelectorAll("li");
+  const text = e.target.value.toLowerCase();
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    if (itemName.indexOf(text) !== -1) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll("li");
+
+  if (items.length === 0) {
+    clearButton.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    clearButton.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+}
+
+//! Event Listeners
+itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearButton.addEventListener("click", clearItems);
+itemFilter.addEventListener("input", filterItems);
+
+checkUI();
